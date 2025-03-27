bf2ca5a3512dd164e952edb7b2a97e2a2285f166
Steve Brodie <shbrodie@gmail.com>
2025-03-27 14:42:21 +1300

    angular 18 update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,25 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-f05c8","appId":"1:596543378105:web:8d599f5ca319cb6bc08168","storageBucket":"friendlychat-f05c8.firebasestorage.app","apiKey":"AIzaSyCIKw44ppyuqYGJja7B8FxfF4yksPJXzzw","authDomain":"friendlychat-f05c8.firebaseapp.com","messagingSenderId":"596543378105","measurementId":"G-SJG7814C7X"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
+
